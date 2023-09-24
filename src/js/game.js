bc95ed08ea111e71f00f6a6e219302c2aafcb8a9
Dmitry Saunin <saunindm@gmail.com>
2023-09-24 22:31:41 +0300

    init commit

---
+++ src/js/game.js
@@ -0,0 +1,23 @@
+import Character from './domain';
+export default Game;
+export class GameSavingData {
+};
+export function readGameSaving() {
+};
+export function writeGameSaving() {
+};
+
+class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  class GameSavingData {
+  }
+  
+  function readGameSaving() {
+  }
+  
+  function writeGameSaving() {
+  }
